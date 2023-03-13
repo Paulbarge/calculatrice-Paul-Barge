8a389448e3a5ccdf57ba14e8062e2ca549760414
Paulbarge <90193040+Paulbarge@users.noreply.github.com>
2023-03-13 22:24:37 +0100

    etpae1

---
+++ index.js
@@ -0,0 +1,9 @@
+function affichage(tmp){
+    document.getElementById("value").value +=tmp;
+}
+function supprimer(){
+    document.getElementById("value").value = ' '
+}
+function calcul(){
+    document.getElementById("value").value = eval(document.getElementById("value").value);
+}
