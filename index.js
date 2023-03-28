9f1c753ef9d51267e39212d160a05eb180a495d8
Paulbarge <paul.barge@student.junia.com>
2023-03-28 10:43:11 +0200

    .

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-function affichage(tmp){
-    document.getElementById("value").value +=tmp;
+class BaseCalculator {
+    constructor() {
+     // Do some stuff
+     }
+    affichage(tmp){
+        document.getElementById("value").value +=tmp;
+    }
+    supprimer(){
+        document.getElementById("value").value = ' '
+    }
+    calcul(){
+        document.getElementById("value").value = eval(document.getElementById("value").value);
+    } 
 }
-function supprimer(){
-    document.getElementById("value").value = ' '
-}
-function calcul(){
-    document.getElementById("value").value = eval(document.getElementById("value").value);
-} 
+
+let baseCalculator = new BaseCalculator();
